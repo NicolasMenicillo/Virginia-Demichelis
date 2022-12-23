ca03a988f6da937302ac652b6d5b6b736c393704
Nicolas Menicillo <nicolasmenicillo@gmail.com>
2022-12-23 16:18:41 -0300

    js

---
+++ practica.js
@@ -0,0 +1,20 @@
+
+const info = alert("Tambien Vendemos Productos Natura!!")
+
+
+let opciones = parseInt(prompt("1- Ver productos\n2- Volver al sitio web"))
+
+switch (opciones){
+    case 1 :
+        alert("Bienvenido a Productos Natura")
+        prompt("Escriba su Email Para recibir los catalogos")
+        break
+
+    case 2: 
+        alert("Disfrute Del Sitio")    
+        break
+    
+    default:
+           alert("Opcion invalida!")
+}
+
